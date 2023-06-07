0d253e6f3ff48ad7eca918399f0d95e969c2fe94
Arthur Andrade <120082347+arthraw@users.noreply.github.com>
2023-06-07 10:54:08 -0300

    Add files via upload

---
+++ Bingo/Bingo/script.js
@@ -0,0 +1,96 @@
+var jogadores = []
+
+function gerarNumerosAleatorios(quantidade, min, max){
+
+    if(quantidade > (max - min)){
+        console.log("Intervalo insuficiente ...");
+        return;
+    }
+
+    var numeros = [];
+
+    while(numeros.length < quantidade){
+        var aleatorio = Math.floor(Math.random()*(max - min) + min);
+        
+        if(!numeros.includes(aleatorio)){
+            numeros.push(aleatorio);
+        }
+    }
+
+    return numeros;
+
+}
+
+function gerarCartela(){
+    var nomeJogador = prompt('Digite o nome do jogador');
+
+    var cartela = [gerarNumerosAleatorios(5,1,15), gerarNumerosAleatorios(5,16,30), gerarNumerosAleatorios(5,31,45),gerarNumerosAleatorios(5,46,60), gerarNumerosAleatorios(5,61,75)]
+
+    jogadores.push({
+        nomeJogador: nomeJogador,
+        cartela: cartela
+    });
+
+    desenharCartela(nomeJogador, cartela);
+
+    console.log(jogadores)
+    
+    return nomeJogador
+}
+
+function reiniciarJogo(){
+    jogadores = []
+}
+
+function desenharCartela(nomeJogador, cartela){
+
+    var div = document.getElementById('espaco_cartelas');
+
+    var tabela = document.createElement('table');
+
+    var thead = document.createElement('thead');
+    
+    var player = document.querySelector('nome-jogador')
+    player.innerHTML = `<p class="jogador-inserido">${nomeJogador}</p>`;
+    document.getElementById("nome-jogador").style = "width: 70%; height: 90vh; display: flex; flex-direction: column; justify-content: center;"
+
+    var nome = document.createElement('p')
+    nome.innerText = `${nomeJogador}`
+
+    var thB = document.createElement('th');
+    thB.innerText = 'B';
+    var thI = document.createElement('th');
+    thI.innerText = 'I';
+    var thN = document.createElement('th');
+    thN.innerText = 'N';
+    var thG = document.createElement('th');
+    thG.innerText = 'G';
+    var thO = document.createElement('th');
+    thO.innerText = 'O';
+
+    thead.appendChild(thB)
+    thead.appendChild(thI)
+    thead.appendChild(thN)
+    thead.appendChild(thG)
+    thead.appendChild(thO)
+
+    nome.appendChild()
+
+    for(var i = 0; i < 5; i++){
+        var tr = document.createElement('tr');
+        for(var j = 0; j < 5; j++){
+            var td = document.createElement('td');
+            if(i == 2 && j == 2){
+                td.innerText = "X";
+                tr.appendChild(td);
+            }else{
+                td.innerText = cartela[j][i]
+                tr.appendChild(td);
+            }
+        }
+        tabela.appendChild(tr)
+    }
+
+    tabela.appendChild(thead);
+    div.appendChild(tabela);
+}
